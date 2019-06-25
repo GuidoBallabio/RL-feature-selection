a5373b27bf0f4686bab8a2eeec692061c936c967
GuidoBallabio <guidodino.ballabio@mail.polimi.it>
2019-06-26 01:17:16 +0200

    steplists added, bound removed

--- code/src/algorithm/backward_feature_selection.py
+++ code/src/algorithm/backward_feature_selection.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from src.algorithm.feature_selection import Bound, FeatureSelector
+from src.algorithm.feature_selection import FeatureSelector
 from tqdm.autonotebook import tqdm
 
 
@@ -10,68 +10,65 @@ class BackwardFeatureSelector(FeatureSelector):
 
         self.idSelected = set(self.idSet)
 
-    def selectNfeatures(self, n, k, gamma, bound=Bound.cmi, show_progress=True):
-        assert k <= self.max_k, f"k {k} is larger than the shortest trajectory (len {self.max_k})"
+    def selectNfeatures(self, n, k, gamma, sampling="frequency", freq=1, show_progress=True):
         assert n <= self.n_features, f"Features to be selected {n} must be less than  the total" \
             f"number of feature: {self.n_features}"
 
         self.reset()
+        steplist, max_t = self._generate_steplist(k, sampling, freq)
 
-        self.weights = self._get_weights(k, gamma, bound)
+        self.weights = self._get_weights_by_steplist(steplist, gamma)
 
         error = 0.0
-        self._prep_data(k)
+        self._prep_data(max_t)
 
         for i in tqdm(range(self.n_features - n), disable=not show_progress):
             scores = self.scoreFeatures(
-                k, gamma, bound, show_progress=show_progress)
+                steplist, gamma, show_progress=show_progress)
 
             self.idSelected.remove(scores[0][0])
             self.residual_error += scores[1][0]
             self.correction_term = scores[2][0]
-            error = self.computeError(bound)
+            error = self.computeError()
 
         return self.idSelected.copy(), error
 
-    def try_remove_all(self, k, gamma, bound=Bound.cmi, show_progress=True):
-        assert k <= self.max_k, f"k {k} is larger than the shortest trajectory (len {self.max_k})"
-
+    def try_remove_all(self, k, gamma, sampling="frequency", freq=1, show_progress=True):
         self.reset()
+        steplist, max_t = self._generate_steplist(k, sampling, freq)
 
-        self.weights = self._get_weights(k, gamma, bound)
+        self.weights = self._get_weights_by_steplist(steplist, gamma)
 
         error = 0.0
-        self._prep_data(k)
+        self._prep_data(max_t)
 
         for i in tqdm(range(self.n_features), disable=not show_progress):
             scores = self.scoreFeatures(
-                k, gamma, bound, show_progress=show_progress)
+                steplist, gamma, show_progress=show_progress)
 
             self.idSelected.remove(scores[0][0])
             self.residual_error += scores[1][0]
             self.correction_term = scores[2][0]
-            error = self.computeError(bound)
+            error = self.computeError()
             yield self.idSelected.copy(), error  # if all is useless move up
 
-    def selectOnError(self, k, gamma, max_error, bound=Bound.cmi, show_progress=True):
-        assert k <= self.max_k, f"k {k} is larger than the shortest trajectory (len {self.max_k})"
-
+    def selectOnError(self, k, gamma, max_error, sampling="frequency", freq=1, show_progress=True):
         self.reset()
+        steplist, max_t = self._generate_steplist(k, sampling, freq)
 
-        self.weights = self._get_weights(k, gamma, bound)
+        self.weights = self._get_weights_by_steplist(steplist, gamma)
 
         error = 0.0
-        self._prep_data(k)
+        self._prep_data(max_t)
 
         with tqdm(total=100, disable=not show_progress) as pbar:  # tqdm
             while error <= max_error and len(self.idSelected) > 1:
                 scores = self.scoreFeatures(
-                    k, gamma, bound, show_progress=show_progress)
+                    steplist, gamma, show_progress=show_progress)
 
                 new_cmi_term = self.residual_error + scores[1][0]
                 new_corr_term = scores[2][0]
-                new_error = self.computeError(
-                    bound, new_cmi_term, new_corr_term)
+                new_error = self.computeError(new_cmi_term, new_corr_term)
 
                 perc_of_max = int(100*new_error/max_error)  # tqdm
                 pbar.update(min(perc_of_max, pbar.total) - pbar.n)  # tqdm
@@ -91,23 +88,24 @@ class BackwardFeatureSelector(FeatureSelector):
     def _scoreFeatureParallel(self):
         pass
 
-    def _scoreFeatureSequential(self, k, gamma, bound, show_progress):
+    def _scoreFeatureSequential(self, steplist, gamma, show_progress):
+        k = len(steplist)
+
         S = frozenset(self.idSelected)
         no_S = self.idSet.difference(self.idSelected)  # discarded
 
         list_ids = np.fromiter(S, dtype=np.int)
         score_mat = np.zeros((k+1, len(list_ids)))
 
-        fun_t, fun_k = self._funOfBound(bound)
-
         for i, id in enumerate(tqdm(list_ids, leave=False, disable=not show_progress)):
             id = frozenset({id})
             S_no_i = S.difference(id)
             no_S_i = no_S.union(id)
 
-            for t in range(k):
-                score_mat[t, i] = fun_t(id, S_no_i, t)
-            score_mat[k, i] = fun_k(no_S_i, S_no_i)
+            for j, t in enumerate(steplist):
+                score_mat[j, i] = self.itEstimator.estimateCMI(
+                    frozenset({self.id_reward}), no_S_i, S_no_i, t=t)
+            score_mat[k, i] = self.itEstimator.estimateCH(no_S_i, S_no_i)
 
         cmi_wsum = np.einsum('a, ab->b', self.weights[:-1], score_mat[:-1, :])
         new_cond_entropy = self.weights[-1] * score_mat[-1, :]
--- code/src/algorithm/feature_selection.py
+++ code/src/algorithm/feature_selection.py
@@ -1,12 +1,9 @@
 import abc
-from enum import Enum
 
 import numpy as np
 
 from src.algorithm.info_theory.it_estimator import CachingEstimator
 from src.algorithm.utils import independent_roll
-
-Bound = Enum("Bound", ['cmi', 'cmi_sqrt', 'entropy'])
 
 
 class FeatureSelector(metaclass=abc.ABCMeta):
@@ -22,37 +19,39 @@ class FeatureSelector(metaclass=abc.ABCMeta):
         self.id_reward = self.n_features
         self.idSet = frozenset(list(range(self.n_features)))
 
-        self.Rmax = np.max([np.max(np.abs(t[:, self.id_reward]))
-                            for t in self.trajectories])
+        self.Rmax = np.max([np.max(np.abs(tr[:, self.id_reward]))
+                            for tr in self.trajectories])
 
-        self.max_k = min(len(t) for t in self.trajectories)
+        self.max_t = min(len(tr) for tr in self.trajectories)
 
         self.residual_error = 0
         self.correction_term = 0
 
-    def _prep_data(self, k):
-        if hasattr(self, 'k_step_data') and k == self.k_step_data.shape[2]:
+    def _prep_data(self, max_t):
+        if hasattr(self, 't_step_data') and max_t + 1 == self.t_step_data.shape[2]:
             return
+
+        assert max_t < self.max_t, f"max timestep {max_t} is not less than the shortest trajectory (len {self.max_t})"
 
         shift = np.zeros(self.n_features + 1, dtype=np.int)
         shift[self.id_reward] = -1
 
-        self.k_step_data = []
-        for t in range(k):
+        self.t_step_data = []
+        for t in range(max_t + 1):
             t_shift = t*shift
             t_step_eps = []
             for ep in self.trajectories:
                 t_step_eps.append(independent_roll(ep, t_shift)[0, :])
 
-            self.k_step_data.append(np.vstack(t_step_eps))
+            self.t_step_data.append(np.vstack(t_step_eps))
 
-        self.k_step_data = np.dstack(self.k_step_data)
+        self.t_step_data = np.dstack(self.t_step_data)
 
     def _get_arrays(self, ids, t):
         if not isinstance(ids, list):
             ids = list(ids)
 
-        return self.k_step_data[:, ids, t]
+        return self.t_step_data[:, ids, t]
 
     def scoreFeatures(self, *args, **kwargs):
         if self.nproc > 1:
@@ -60,60 +59,47 @@ class FeatureSelector(metaclass=abc.ABCMeta):
         else:
             return self._scoreFeatureSequential(*args, **kwargs)
 
-    def _get_weights(self, k, gamma, bound):
-        if bound is not Bound.cmi_sqrt:
-            gamma = gamma**2
+    def _generate_steplist(self, k, sampling, freq):
+        if sampling == "frequency":
+            max_t = (k-1) * freq
+            assert max_t <= self.max_t, f"max timestep {max_t} is larger than the shortest trajectory (len {self.max_t})"
+            return np.arange(k*freq, step=freq), max_t
 
-        weights = np.ones(k+1) * gamma
-        weights = weights ** np.arange(k+1)
-            
-        if bound is Bound.entropy:
-            weights[k] = 1/(1 - gamma)
-        else:
-            weights[k] /= 1 - gamma
+        if sampling == "decaying":
+            p = np.exp(-np.arange(self.max_t)/freq) / freq
+            p = p/p.sum()
+            steplist = np.sort(np.random.choice(
+                self.max_t, size=k, replace=False, p=p))
+            return steplist, steplist[-1]
+
+    def _get_weights_by_steplist(self, steplist, gamma):
+        k = len(steplist)
+        gamma = gamma**2
+
+        weights = np.ones(k + 1) * gamma
+        weights[:-1] = weights[:-1] ** steplist
+
+        weights[k] = 1/(1 - gamma) - weights[:-1].sum()
 
         return weights
 
-    def _funOfBound(self, bound):
-        if bound is Bound.cmi_sqrt:
-            def fun_t(no_S_i, S_no_i, t): return np.sqrt(self.itEstimator.estimateCMI(
-                frozenset({self.id_reward}), no_S_i, S_no_i, t=t))
-
-            def fun_k(no_S_i, S_no_i): return np.sqrt(
-                self.itEstimator.estimateCH(no_S_i, S_no_i))
-        elif bound is Bound.cmi:
-            def fun_t(no_S_i, S_no_i, t): return self.itEstimator.estimateCMI(
-                frozenset({self.id_reward}), no_S_i, S_no_i, t=t)
-
-            def fun_k(no_S_i, S_no_i): return self.itEstimator.estimateCH(
-                no_S_i, S_no_i)
-        elif bound is Bound.entropy:
-            def fun_t(no_S_i, S_no_i, t): return -self.itEstimator.estimateCH(no_S_i,
-                                        frozenset({self.id_reward}).union(S_no_i), t=t)
-
-            def fun_k(no_S_i, S_no_i): return self.itEstimator.estimateCH(
-                no_S_i, S_no_i)
-
-        return fun_t, fun_k
-
-    def computeError(self, bound=Bound.cmi, residual=None, correction=None):
+    def computeError(self, residual=None, correction=None):
         if residual is None:
             residual = self.residual_error
         if correction is None:
             correction = self.correction_term
 
-        if bound is Bound.cmi_sqrt:
-            return 2**(1/2) * self.Rmax * residual
         return 2**(1/2) * self.Rmax * np.sqrt(residual + correction)
 
     def reset(self):
         self.residual_error = 0
         self.correction_term = 0
+        self.weights = None
 
     @abc.abstractmethod
-    def selectOnError(self, k, gamma, max_error, bound=Bound.entropy, show_progress=True):
+    def selectOnError(self, k, gamma, max_error, show_progress=True):
         pass
 
     @abc.abstractmethod
-    def selectNfeatures(self, n, k, gamma, bound=Bound.cmi, show_progress=True):
+    def selectNfeatures(self, n, k, gamma, show_progress=True):
         pass
